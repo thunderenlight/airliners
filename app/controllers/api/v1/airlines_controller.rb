ad13b3895f4fe87008c391f15cab376b9dedf2f2
thunderenlight <thunderenlight@gmail.com>
2020-09-16 23:31:10 -0500

    Adding API methods

---
+++ app/controllers/api/v1/airlines_controller.rb
@@ -0,0 +1,35 @@
+
+module Api
+	module V1
+		class AirlinesController < ApplicationController
+			def index
+				airlines = Airline.all 
+
+				render json: AirlineSerializer.new(airlines).serialized_json
+			end
+
+			def show
+				airline = Airline.find_by(slug: params[:slug])
+
+				render json: AirlineSerializer.new(airlines).serialized_json
+			end
+
+			def create
+				airline = Airline.new(airline_params)
+
+				if airline.save
+					render json: AirlineSerializer.new(airline).serialized_json
+				else
+					render json: { error: airline.error.messages }, status: 422
+				end
+				
+			end
+
+			private
+
+			def airlines_params
+				params.require(:airline).permit(:name, :image_url)
+			end
+		end
+	end
+end
---
+++ app/models/airline.rb
@@ -0,0 +1,16 @@
+class Airline < ApplicationRecord
+	has_many :reviews
+
+	before_create :slugify
+
+	def slugify
+		self.slug = name.parameterize
+	end
+
+
+	def avg_score
+		return 0 unless reviews.size.positive?
+
+		reviews.average(:score).to_f.round(2)
+	end
+end
