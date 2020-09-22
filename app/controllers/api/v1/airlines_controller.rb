6cf3d2b962956ff0e097da641183c612f4796a45
thunderenlight <thunderenlight@gmail.com>
2020-09-21 19:59:50 -0500

    Adding API requests

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -2,16 +2,18 @@
 module Api
 	module V1
 		class AirlinesController < ApplicationController
+			protect_from_forgery with: :null_session
+
 			def index
 				airlines = Airline.all 
 
-				render json: AirlineSerializer.new(airlines).serialized_json
+				render json: AirlineSerializer.new(airlines, options).serialized_json
 			end
 
 			def show
 				airline = Airline.find_by(slug: params[:slug])
 
-				render json: AirlineSerializer.new(airlines).serialized_json
+				render json: AirlineSerializer.new(airline, options).serialized_json
 			end
 
 			def create
@@ -22,13 +24,38 @@ module Api
 				else
 					render json: { error: airline.error.messages }, status: 422
 				end
-				
+
+			end
+
+			def update
+				airline = Airline.find_by(slug: params[:slug])
+
+				if airline.update(airline_params)
+					render json: AirlineSerializer.new(airline, options).serialized_json
+				else
+					render json: {error: airline.errors.messages }, status: 422
+				end
+
+			end
+
+			def destroy
+				airline = Airline.find_by(slug: params[:slug])
+
+				if airline.destroy
+					head :no_content
+				else
+					render json: { errors: airline.errors.messages }, status: 422
+				end
 			end
 
 			private
 
-			def airlines_params
+			def airline_params
 				params.require(:airline).permit(:name, :image_url)
+			end
+
+			def options
+				@options ||= { include: %i[reviews] }
 			end
 		end
 	end
